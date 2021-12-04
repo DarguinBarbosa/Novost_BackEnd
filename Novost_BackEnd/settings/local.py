23222aca0b6efb1cf234e982d3c059d139c73fd4
Darguin_Barbosa <darguinbarbosa22@gmail.com>
2021-12-04 11:43:59 -0500

    Ajustes correo

--- Novost_BackEnd/settings/local.py
+++ Novost_BackEnd/settings/local.py
@@ -19,6 +19,13 @@ DATABASES = {
     }
 }
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "senanovost@gmail.com"
+EMAIL_HOST_PASSWORD = "SistemaNovost123"
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
