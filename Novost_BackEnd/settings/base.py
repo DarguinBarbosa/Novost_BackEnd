102a12836a3eb1c8ce9413ae996907fb7f77a91b
Darguin_Barbosa <82847226+DarguinBarbosa@users.noreply.github.com>
2021-12-03 22:52:04 -0500

    A

--- Novost_BackEnd/settings/base.py
+++ Novost_BackEnd/settings/base.py
@@ -79,11 +79,8 @@ TEMPLATES = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES':[
-        'rest_framework.permissions.IsAuthenticated'
-    ],
     'DEFAULT_AUTHENTICATION_CLASSES':[
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
 
