319775056e3ddc78e838031c660c8ee81896f1c9
Darguin_Barbosa <82847226+DarguinBarbosa@users.noreply.github.com>
2021-12-03 20:35:33 -0500

    Update base.py

--- Novost_BackEnd/settings/base.py
+++ Novost_BackEnd/settings/base.py
@@ -1,25 +1,25 @@
-importar os
-desde pathlib import path
+import os
+from pathlib import Path
 
-# Construye rutas dentro del proyecto de esta manera: BASE_DIR / 'subdir'.
-BASE_DIR = Ruta(__file__). resolver(). padre. padre
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 MEDIA_URL = '/solicitudes/'
-MEDIA_ROOT = os. ruta. join(BASE_DIR, '.. /solicitudes')
+MEDIA_ROOT = os.path.join(BASE_DIR, '../solicitudes')
 
-# Configuración de desarrollo de inicio rápido: no es adecuada para la producción
-# Ver https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-X_FRAME_OPTIONS='permitir-desde*'
-# ADVERTENCIA DE SEGURIDAD: ¡mantenga en secreto la clave secreta utilizada en la producción!
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+X_FRAME_OPTIONS='allow-from*'
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-a%x(lw#5km*v@jv78yx_h&5%h1rbubekuo_db@cb=7(ibn+jyq'
 
-# ADVERTENCIA DE SEGURIDAD: ¡no se ejecute con la depuración activada en producción!
-DEPURAR = Falso
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
 
-# Definición de la aplicación
+# Application definition
 
 BASE_APPS = [
     'django.contrib.admin',
@@ -30,7 +30,7 @@ BASE_APPS = [
     'django.contrib.staticfiles',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = Verdadero
+CORS_ORIGIN_ALLOW_ALL = True
 
 LOCAL_APPS = [
     'apps.users',
@@ -62,15 +62,15 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'Novost_BackEnd.urls'
 
-PLANTILLAS = [
+TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
- «DIRS»: [],
- 'APP_DIRS': Verdadero,
- 'OPCIONES': {
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
             'context_processors': [
- 'django.template.context_processors.debug',
- «django.template.context_processors.request»,
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -81,58 +81,58 @@ PLANTILLAS = [
 WSGI_APPLICATION = 'Novost_BackEnd.wsgi.application'
 
 
-# Validación de contraseña
+# Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
- 'NOMBRE': 'django.contrib.auth.password_validation. UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
- 'NOMBRE': 'django.contrib.auth.password_validation. MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
- 'NOMBRE': 'django.contrib.auth.password_validation. CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
- 'NOMBRE': 'django.contrib.auth.password_validation. NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
 
-# Internacionalización
+# Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'es-co'
 
 TIME_ZONE = 'UTC'
 
-USE_I18N = Verdadero
+USE_I18N = True
 
-USE_L10N = Verdadero
+USE_L10N = True
 
-USE_TZ = Verdadero
+USE_TZ = True
 
-AUTH_USER_MODEL = 'usuarios. Usuario'
+AUTH_USER_MODEL = 'users.User'
 
-# Archivos estáticos (CSS, JavaScript, Imágenes)
+# Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/estático/'
+STATIC_URL = '/static/'
 
-# Tipo de campo de clave principal predeterminado
+# Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_USE_TLS = Verdadero
+EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "senanovost@gmail.com"
 EMAIL_HOST_PASSWORD = "SistemaNovost123"
 
 
-# Configurar la aplicación Django para Heroku.
-django_heroku de importación
-django_heroku. configuración(locales()) 
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals()) 
