0dc92c56c2192978ca16a62d69d0849e5e885b6a
Darguin_Barbosa <82847226+DarguinBarbosa@users.noreply.github.com>
2021-12-03 21:14:27 -0500

    Update base.py

--- Novost_BackEnd/settings/base.py
+++ Novost_BackEnd/settings/base.py
@@ -78,6 +78,10 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES':['rest_framework.permissions.IsAuthenticated']
+}
+
 WSGI_APPLICATION = 'Novost_BackEnd.wsgi.application'
 
 
