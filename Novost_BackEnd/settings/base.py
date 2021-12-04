e329f129de5dd1f06e275d1024eb8df3b1c164e0
Darguin_Barbosa <82847226+DarguinBarbosa@users.noreply.github.com>
2021-12-03 23:56:15 -0500

    Update base.py

--- Novost_BackEnd/settings/base.py
+++ Novost_BackEnd/settings/base.py
@@ -79,10 +79,9 @@ TEMPLATES = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    )
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication', 
+    ],
 }
 
 WSGI_APPLICATION = 'Novost_BackEnd.wsgi.application'
