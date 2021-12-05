88e5e90946b8ac4acf8a430e25f886062be8d35c
Darguin_Barbosa <darguinbarbosa22@gmail.com>
2021-12-04 21:54:56 -0500

    Ajustando  Archivos

--- Novost_BackEnd/settings/base.py
+++ Novost_BackEnd/settings/base.py
@@ -4,7 +4,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-MEDIA_URL = '/static/'
+MEDIA_URL = '../static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '../static')
 
 # Quick-start development settings - unsuitable for production
