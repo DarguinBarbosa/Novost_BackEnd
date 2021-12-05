14c35b0e261668276b7b38374084cb25d4053734
Darguin_Barbosa <darguinbarbosa22@gmail.com>
2021-12-04 22:05:22 -0500

    Ajustando  Archivos

--- Novost_BackEnd/settings/base.py
+++ Novost_BackEnd/settings/base.py
@@ -129,7 +129,7 @@ AUTH_USER_MODEL = 'users.User'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
