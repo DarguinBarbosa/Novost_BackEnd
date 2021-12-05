b380115ea969dac16a67834c4cade51cc5ee256d
Darguin_Barbosa <darguinbarbosa22@gmail.com>
2021-12-05 00:06:46 -0500

    Ajustando  Archivos

--- Novost_BackEnd/urls.py
+++ Novost_BackEnd/urls.py
@@ -1,4 +1,3 @@
-from typing import Pattern
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
@@ -12,7 +11,6 @@ urlpatterns = [
     path('', Login.as_view(), name = 'Login'),
     path('Logout', LogOut.as_view(), name = 'Logout'),
     path('novedades/', include('apps.novedades.api.urls')),
-] 
-urlpatterns += Pattern('' (r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),)
+] +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # urlpatterns += static(settings.MEDIA_ROOT)
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
