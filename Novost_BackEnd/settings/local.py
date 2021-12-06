dcc22f861730d8b5da3d0df1979ec1f3b26768c9
Darguin_Barbosa <darguinbarbosa22@gmail.com>
2021-12-06 00:31:51 -0500

    Correo Mejorado

--- Novost_BackEnd/settings/local.py
+++ Novost_BackEnd/settings/local.py
@@ -24,7 +24,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "senanovost@gmail.com"
-EMAIL_HOST_PASSWORD = "jdywyuhalcojshvu"
+EMAIL_HOST_PASSWORD = "SistemaNovost123"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
