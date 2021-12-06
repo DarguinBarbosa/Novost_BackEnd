33aa0ada802a59523d23031fd5ccc7e98f0d8fc6
Darguin_Barbosa <darguinbarbosa22@gmail.com>
2021-12-06 02:18:26 -0500

    R proyecto

--- Novost_BackEnd/settings/base.py
+++ Novost_BackEnd/settings/base.py
@@ -141,8 +141,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "senanovost@gmail.com"
-EMAIL_HOST_PASSWORD = "pcfebyiyinnfvton"
+EMAIL_HOST_USER = "sistemanovost@gmail.com"
+EMAIL_HOST_PASSWORD = "miaqstxrahytdikt"
 
 STATIC_ROOT =os.path.join(BASE_DIR,'../staticfiles')
 STATIC_URL = '/static/'
--- Novost_BackEnd/settings/local.py
+++ Novost_BackEnd/settings/local.py
@@ -23,8 +23,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "senanovost@gmail.com"
-EMAIL_HOST_PASSWORD = "pcfebyiyinnfvton"
+EMAIL_HOST_USER = "sistemanovost@gmail.com"
+EMAIL_HOST_PASSWORD = "miaqstxrahytdikt"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
